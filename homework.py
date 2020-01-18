bb816d4667a0dd02d3dc65b3e6087e4280f5c069
Emilio Araos <ea3dev@gmail.com>
2020-01-18 02:12:05 -0700

    solution

---
+++ homework.py
@@ -0,0 +1,41 @@
+#Handle the exception thrown by the code below by using try and except blocks.
+for i in ['a', 'b', 'c']:
+	try:
+		print(i **2)
+	except TypeError:
+		print("THere was an error. Fix the code")
+	finally:
+		print("Check the documentation")
+
+#############Handle the exception thrown by the code below by using try and except blocks. Then use a finally block to print 'All Done.'
+
+x = 5
+y = 0
+
+try:	
+	z = x/y
+except ZeroDivisionError:
+	print('You are trying to divide by zero. Can not do that')
+finally:
+	print('All done')
+
+###########Write a function that asks for an integer and prints the square of it. Use a while loop with a try, except, else block to account for incorrect inputs.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
