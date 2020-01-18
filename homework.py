8cd7450c9424eb9c0f854758ebae537da4c15e84
Emilio Araos <ea3dev@gmail.com>
2020-01-18 02:15:17 -0700

    fixed identation

--- homework.py
+++ homework.py
@@ -1,18 +1,19 @@
 #Handle the exception thrown by the code below by using try and except blocks.
-for i in ['a', 'b', 'c']:
-	try:
+try:
+	for i in ['a', 'b', 'c']:
 		print(i **2)
-	except TypeError:
-		print("THere was an error. Fix the code")
-	finally:
-		print("Check the documentation")
+except TypeError:
+	print("THere was an error. Fix the code")
+finally:
+	print("Check the documentation")
 
 #############Handle the exception thrown by the code below by using try and except blocks. Then use a finally block to print 'All Done.'
 
-x = 5
-y = 0
 
-try:	
+
+try:
+	x = 5
+	y = 0	
 	z = x/y
 except ZeroDivisionError:
 	print('You are trying to divide by zero. Can not do that')
