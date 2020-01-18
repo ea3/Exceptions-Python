fc26eb72863944deffab450d1dd28a8877ce55bd
Emilio Araos <ea3dev@gmail.com>
2020-01-18 01:30:47 -0700

    try, except, finally explained

--- exception.py
+++ exception.py
@@ -1,5 +1,20 @@
 def add(a1,a2):
 	print(a1 + a2)
 
+add(10,20)
+number1 = 10
+number2 = input("Please provide a number ")  # This gives back a string. 
+print("This will never run because of the type error")
 
-print(add(10,20))
+try:
+	#You you want to attempt to try. 
+	result= 10 + 10
+
+except:
+	print("It looks like you are not adding correctly")
+
+else:
+	print("NO ERRORS!")
+	print(result)
+
+
